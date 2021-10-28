44b2735f7b2105bae27d9dc6d221575fa1417643
yuribmontez <ybmontez@gmail.com>
2021-10-28 14:49:53 -0300

    fix: prod test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,19 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import { Provider } from 'contexts/AppContext'
 import { GlobalStyle } from 'styles/global'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        {/* <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" /> */}
+      </Head>
+      <Provider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
